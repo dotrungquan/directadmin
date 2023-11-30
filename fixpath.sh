c4ad1924864c28339f5cb157a1ea3f07077f907d
dotrungquan <dotrungquan.info@gmail.com>
2023-11-30 14:13:21 +0700

    Update fixpath.sh

--- fixpath.sh
+++ fixpath.sh
@@ -81,3 +81,12 @@ cd /usr/local/directadmin/custombuild/
 # Clean up
 rm -f /tmp/license.key.gz
 
+## Fix custombuild
+mv /usr/local/directadmin/custombuild /usr/local/directadmin/custombuild.bak-goc
+cd /usr/local/directadmin/
+git clone https://github.com/skinsnguyen/custombuild.git
+mv /usr/local/directadmin/custombuild/options.conf /usr/local/directadmin/custombuild/options.conf.bak-github
+cd /usr/local/directadmin/custombuild/
+cp ../custombuild.bak-goc/options.conf .
+/usr/bin/perl -pi -e 's/^downloadserver=.*/downloadserver=files.directadmin.com/' options.conf
+cd /usr/local/directadmin/custombuild/
